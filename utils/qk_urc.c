7ddeb69ea7d50374ec4e4fce4498f09681de997d
jianxin <jianxin@oradar.com.cn>
2023-10-19 18:25:32 +0800

    优化URC功能

--- utils/qk_urc.c
+++ utils/qk_urc.c
@@ -4,6 +4,9 @@
 
 static LIST_HEAD(urc_info_list_head);
 
+static volatile void *urc_feed_data = NULL;
+static volatile uint16_t urc_feed_len = 0;
+
 struct urc_info* qk_urc_info_find(struct urc_info* info)
 {
     struct list_head *pos   = NULL;
@@ -20,6 +23,37 @@ struct urc_info* qk_urc_info_find(struct urc_info* info)
     return u_info;
 }
 
+struct urc_info* qk_urc_info_init(struct urc_info* info, const char *prefix, const char *suffix, fn_urc_cb_t cb)
+{
+    if (info == NULL || cb == NULL)
+    {
+        return NULL;
+    }
+
+    if (qk_urc_info_find(info) != NULL)
+    {
+        log_w("urc info <0x%08x>[%s, %s] exsit!\r\n", (uint32_t)info, prefix, suffix);
+        return info;
+    }
+    info->cmd_prefix = prefix;
+    info->cmd_suffix = suffix;
+    info->callback = cb;
+
+    list_add(&info->list, &urc_info_list_head);
+
+    log_d("initial <0x%08x>[%s, %s] success!\r\n", (uint32_t)info, prefix, suffix);
+
+    return info;
+}
+
+void qk_urc_info_feed(void *data, uint16_t len)
+{
+    urc_feed_data = data;
+    urc_feed_len = len;
+}
+
+#include <qk_section.h>
+
 void qk_urc_info_print(void)
 {
     struct list_head *pos   = NULL;
@@ -31,40 +65,63 @@ void qk_urc_info_print(void)
     }
 }
 
-struct urc_info* qk_urc_info_init(struct urc_info* info, const char *prefix, const char *suffix, fn_urc_cb_t cb)
+void info1_func(const char *data, uint16_t size)
 {
-    if (info == NULL )// || cb == NULL)
-    {
-        return NULL;
-    }
-
-    if (qk_urc_info_find(info) != NULL)
-    {
-        return info;
-    }
-    info->cmd_prefix = prefix;
-    info->cmd_suffix = suffix;
-
-    list_add(&info->list, &urc_info_list_head);
-
-    log_d("initial <0x%08x>[%s, %s] success!\r\n", (uint32_t)info, prefix, suffix);
-
-    return info;
+    log_i("%s\r\n", data);
 }
+struct urc_info info1;
+struct urc_info info2;
+struct urc_info info3;
 
-#include <qk_section.h>
-fn_urc_cb_t tese;
 void test_urc(void)
 {
-    struct urc_info info1;
-    struct urc_info info2;
-    struct urc_info info3;
-    
-    qk_urc_info_init(&info1, "123", "234", NULL);
+    qk_urc_info_init(&info1, "123", "234", info1_func);
     qk_urc_info_init(&info2, "111", "222", NULL);
     qk_urc_info_init(&info3, "444", "000", NULL);
     qk_urc_info_init(&info3, "333", "777", NULL);
     qk_urc_info_print();
+    
+    qk_urc_info_feed("123-ccc-234", 11);
 }
 
 INITLV0_EXPORT(test_urc);
+
+void qk_urc_info_handler(void)
+{
+    if (urc_feed_data == NULL)
+    {
+        goto __handler_fail;
+    }
+    
+    uint8_t prefix_len, suffix_len;
+    
+    struct list_head *pos   = NULL;
+    struct urc_info     *u_info = NULL;
+    list_for_each(pos, &urc_info_list_head)
+    {
+        u_info = list_entry(pos, struct urc_info, list);
+        
+        prefix_len = strlen(u_info->cmd_prefix);
+        suffix_len = strlen(u_info->cmd_suffix);
+        if (urc_feed_len < prefix_len + suffix_len)
+        {
+            continue;
+        }
+        if ((prefix_len ? !strncmp((char *)urc_feed_data, u_info->cmd_prefix, prefix_len) : 1)
+			&& (suffix_len ? !strncmp((char *)urc_feed_data + urc_feed_len - suffix_len, u_info->cmd_suffix, suffix_len) : 1))
+        {
+            if(u_info->callback)
+			{
+				memset((char *)urc_feed_data + urc_feed_len - suffix_len, 0, suffix_len);
+				urc_feed_len = urc_feed_len - prefix_len - suffix_len;
+				u_info->callback((char *)urc_feed_data + prefix_len, urc_feed_len);
+			}
+        }
+    }
+    
+__handler_fail:
+    urc_feed_data = NULL;
+    urc_feed_len = 0;
+}
+
+POLLING_EXPORT(qk_urc_info_handler);

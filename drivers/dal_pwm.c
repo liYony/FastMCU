62a4f0f708f65c723b5e926e8e424a9471596672
liYony <941843540@qq.com>
2023-10-26 22:58:36 +0800

    优化PWM框架

--- drivers/dal_pwm.c
+++ drivers/dal_pwm.c
@@ -1,6 +1,6 @@
 #include <dal_pwm.h>
 
-dal_weak int mcu_pwm_init(dal_pwm_number_t pwm, uint32_t period, uint32_t pulse)
+dal_weak int mcu_pwm_init(dal_pwm_number_t pwm, uint32_t freq, uint32_t period)
 {
     return -1;
 }
@@ -15,9 +15,19 @@ dal_weak int mcu_pwm_disable(dal_pwm_number_t pwm, dal_pwm_channel_t ch)
     return -1;
 }
 
-int dal_pwm_init(dal_pwm_number_t pwm, uint32_t period, uint32_t pulse)
+dal_weak int mcu_pwm_set_period(dal_pwm_number_t pwm, uint32_t period)
 {
-    return mcu_pwm_init(pwm, period, pulse);
+    return -1;
+}
+
+dal_weak int mcu_pwm_set_pulse(dal_pwm_number_t pwm, uint32_t pulse)
+{
+    return -1;
+}
+
+int dal_pwm_init(dal_pwm_number_t pwm, uint32_t freq, uint32_t period)
+{
+    return mcu_pwm_init(pwm, freq, period);
 }
 
 int dal_pwm_enable(dal_pwm_number_t pwm, dal_pwm_channel_t ch)
@@ -28,4 +38,14 @@ int dal_pwm_enable(dal_pwm_number_t pwm, dal_pwm_channel_t ch)
 int dal_pwm_disable(dal_pwm_number_t pwm, dal_pwm_channel_t ch)
 {
     return mcu_pwm_disable(pwm, ch);
+}
+
+int dal_pwm_set_period(dal_pwm_number_t pwm, uint32_t period)
+{
+    return mcu_pwm_set_period(pwm, period);
+}
+
+int dal_pwm_set_pulse(dal_pwm_number_t pwm, uint32_t pulse)
+{
+    return mcu_pwm_set_pulse(pwm, pulse);
 }

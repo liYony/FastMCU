f41ce84647c6db08de4226ee3a133f8a300d6320
liYony <941843540@qq.com>
2023-11-14 22:33:27 +0800

    [stm32][f407] 支持stm32f407

---
+++ bsp/stm32/stm32f407zg/drivers/mcu_uart.c
@@ -0,0 +1,134 @@
+#include <dal.h>
+#include "stm32f4xx_hal.h"
+
+UART_HandleTypeDef huart1;
+
+/* USART2 init function */
+
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 230400;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    return;
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef* huart)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(huart->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspInit 0 */
+
+  /* USER CODE END USART1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_USART1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* USART1 interrupt Init */
+    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART1_IRQn);
+  /* USER CODE BEGIN USART1_MspInit 1 */
+
+  /* USER CODE END USART1_MspInit 1 */
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
+{
+  if(huart->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspDeInit 0 */
+
+  /* USER CODE END USART1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
+
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9|GPIO_PIN_10);
+
+    /* USART1 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(USART1_IRQn);
+  /* USER CODE BEGIN USART1_MspDeInit 1 */
+
+  /* USER CODE END USART1_MspDeInit 1 */
+  }
+}
+
+void uart_send_nbyte(uint8_t *bf, uint16_t len)
+{
+    HAL_UART_Transmit(&huart1, bf, len, 100);
+}
+
+int mcu_uart_init(dal_uart_number_t uart, uint32_t band)
+{
+    if (uart == DAL_UART_1)
+    {
+        MX_USART1_UART_Init();
+        return 0;
+    }
+
+    return -1;
+}
+
+int mcu_uart_deinit(dal_uart_number_t uart)
+{
+    return -1;
+}
+
+int mcu_uart_send(dal_uart_number_t uart, const uint8_t *pbuf, uint16_t len)
+{
+    if (uart == DAL_UART_1)
+    {
+        uart_send_nbyte((uint8_t *)pbuf, len);
+        return 0;
+    }
+
+    return -1;
+}
+
+int mcu_uart_receive(dal_uart_number_t uart, uint8_t *pbuf, uint16_t len)
+{
+    if (uart == DAL_UART_1)
+    {
+        if (HAL_UART_Receive(&huart1, pbuf, len, 10) == HAL_OK)
+            return 0;
+        return -1;
+    }
+
+    return -1;
+}

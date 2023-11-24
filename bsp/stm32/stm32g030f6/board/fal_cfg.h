c881ca6aa01fbdac517656ceef2a68ff37f4814e
jianxin <jianxin@oradar.com.cn>
2023-11-24 14:50:24 +0800

    [fal] 移植成功并且验证成功

--- bsp/stm32/stm32g030f6/board/fal_cfg.h
+++ bsp/stm32/stm32g030f6/board/fal_cfg.h
@@ -24,10 +24,10 @@ extern const struct fal_flash_dev stm32_onchip_flash;
 /* ====================== Partition Configuration ========================== */
 #ifdef FAL_PART_HAS_TABLE_CFG
 /* partition table */
-#define FAL_PART_TABLE                                                               \
-{                                                                                    \
-    {FAL_PART_MAGIC_WORD,        "bl",     "stm32_onchip",         0,   12*1024, 0}, \
-    {FAL_PART_MAGIC_WORD,       "app",     "stm32_onchip",   12*1024,  48*1024,  0}, \
+#define FAL_PART_TABLE                                                                \
+{                                                                                     \
+    {FAL_PART_MAGIC_WORD,       "test1",     "onchip_flash",   40*1024,  12*1024, 0}, \
+    {FAL_PART_MAGIC_WORD,       "test2",     "onchip_flash",   52*1024,  12*1024, 0}, \
 }
 #endif /* FAL_PART_HAS_TABLE_CFG */
 
